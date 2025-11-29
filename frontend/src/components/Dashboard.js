05547334284d16c4ced3d45e169028097d4168bf
gerardoRomero <romerom.gerardo@officedepot.com.mx>
2025-11-29 03:08:04 -0600

    update import

--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import config from "./config.js";
+import config from "../config.js";
 import {
   Chart as ChartJS,
   CategoryScale,
