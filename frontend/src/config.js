1e1264066a0a2a647cbc03274b1918712d61ef12
gerardoRomero <romerom.gerardo@officedepot.com.mx>
2025-11-29 04:00:37 -0600

    revert config base url

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,6 +1,6 @@
 // frontend/src/config.js
 const config = {
-  API_BASE_URL: 'https://puntoventa-happi.onrender.com/api'
+  API_BASE_URL: 'https://puntoventa-happi.onrender.com/'
 };
 
 export default config;
